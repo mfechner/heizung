37b2925d90da6e694c409a858c8dd4dc95ce69f6
Matthias Fechner <idefix@fechner.net>
2013-04-24 00:57:40 +0200

    Added function to store values to database.

--- bin/WPgetAllValues.pl
+++ bin/WPgetAllValues.pl
@@ -5,9 +5,12 @@
 # (c) by Matthias Fechner <idefix@fechner.net>
 #
 use strict;
-use Data::Dumper;
+use DBI;
 use WPMemoryMapping;
 use vars qw(%wp_memory %dataTypes);
+
+my $dbh = DBI->connect('DBI:mysql:heizung:localhost', 'root', 'reset62',
+                    {RaiseError => 0, AutoCommit => 1}) or die "Cannot connect to database" . DBI->errstr;
 
 my $prog;
 my %values;
@@ -20,8 +23,34 @@ foreach(keys %wp_memory) {
     $values{$_}=<PROG>;
     chomp($values{$_});
     close(PROG);
+    print "$_=$values{$_}\n";
 }
 
-foreach(keys %values) {
-	print $_ . ": " . $wp_memory{$_}{caption} . " = " . $values{$_} . $wp_memory{$_}{unit} . "\n";
-}
+print("Store to database\n");
+
+my $query = &create_insert($dbh, "wp_data", \%values);
+my $sth = $dbh->prepare($query) or die("Could not prepare string: ". $dbh->errstr);
+$sth->execute() or die "Couldn't execute statement: " . $sth->errstr;
+
+
+
+# given a DBI object (to do quoting), the table name and a hashref of 
+# field => value pairs, creates an SQL insert statement
+sub create_insert {
+ 
+    my ($dbh, $table, $data) = @_;
+ 
+    if (!$table || ref $data ne 'HASH') {
+        warn "create_insert() called wrongly";
+        return;
+    }
+ 
+    my $sql = sprintf 'INSERT INTO %s (%s) VALUES (%s);',
+        $table,
+        join(', ', map { $dbh->quote_identifier($_) } keys   %$data),
+        join(',' , map { $dbh->quote($_)            } values %$data)
+    ;
+ 
+    return $sql . "\n";
+} # end of function create_insert
+
