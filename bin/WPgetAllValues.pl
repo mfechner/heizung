8031e02ddbc76ddc86ed730967cc5cd7d5067436
Matthias Fechner <idefix@fechner.net>
2013-04-30 15:24:31 +0200

    Moved config to a seperate config file.

--- bin/WPgetAllValues.pl
+++ bin/WPgetAllValues.pl
@@ -6,23 +6,34 @@
 #
 use strict;
 use DBI;
+use Data::Dumper;
+use Config::General;
 use WPMemoryMapping;
 use WPgeneralFunctions;
-use vars qw(%wp_memory %dataTypes);
 
-my $dbh = DBI->connect('DBI:mysql:heizung:localhost', 'root', 'reset62',
-                    {RaiseError => 0, AutoCommit => 1}) or die "Cannot connect to database" . DBI->errstr;
+use vars qw(%wp_memory %dataTypes %config);
+
+my $conf   = new Config::General("config");
+my %config = $conf->getall;
+
+my $dbh = DBI->connect(
+	'DBI:mysql:' . $config{'database'} . ':' . $config{'server'},
+	$config{'user'}, $config{'password'},
+	{ RaiseError => 0, AutoCommit => 1 }
+  )
+  or die "Cannot connect to database" . DBI->errstr;
 
 my $prog;
 my %values;
-foreach(keys %wp_memory) {
-    $values{$_}=&readParameter(\%wp_memory, \%dataTypes, $_);
-    print "$_=$values{$_}\n";
+foreach ( keys %wp_memory ) {
+	$values{$_} = &readParameter( \%wp_memory, \%dataTypes, $_ );
+	#print "$_=$values{$_}\n";
 }
 
-print("Store to database\n");
+#print("Store to database\n");
 
-my $query = &create_insert($dbh, "wp_data", \%values);
-my $sth = $dbh->prepare($query) or die("Could not prepare string: ". $dbh->errstr);
+my $query = &create_insert( $dbh, "wp_data", \%values );
+my $sth = $dbh->prepare($query)
+  or die( "Could not prepare string: " . $dbh->errstr );
 $sth->execute() or die "Couldn't execute statement: " . $sth->errstr;
 
