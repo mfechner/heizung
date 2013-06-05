05070ca942d7af1c32b571b07291147c3fec76f6
Matthias Fechner <idefix@fechner.net>
2013-06-05 18:02:30 +0200

    Added format definitions to perl config hash and use it to format string sent to javascript.

--- frontend/post.php
+++ frontend/post.php
@@ -1,6 +1,9 @@
 <?php
 require_once("_includes/globals.inc.php");
 
+$json = file_get_contents("config.json");
+$config = json_decode($json, TRUE);
+
 $query = "SELECT timestamp,
     			   FirmwareVersion,
                    FirmwareDate,
@@ -41,56 +44,45 @@ $query = "SELECT timestamp,
                    COP
     FROM ".$db_getTable;
 
-// sometimes tsp is newer than db_entry which causes an empty result set
-// so give it a chance otherwise take the newest entry
-if(key_exists('START', $_REQUEST)) {
-    $restriction = " WHERE timestamp > '".$_REQUEST['START']."' ORDER BY id ASC LIMIT 1";
-} else {
-    $restriction = " ORDER BY id DESC LIMIT 1";
+$values = getDbRecord($query);
+$values = formatValues($values);
+echo json_encode($values);
+
+function getDbRecord($query) {
+	global $db_host, $db_user, $db_password, $db_name;
+	// sometimes tsp is newer than db_entry which causes an empty result set
+	// so give it a chance otherwise take the newest entry
+	if(key_exists('START', $_REQUEST) && $_REQUEST['START'] > 0) {
+		$restriction = " WHERE timestamp < '".$_REQUEST['START']."' ORDER BY id DESC LIMIT 1";
+	} else {
+		$restriction = " ORDER BY id DESC LIMIT 1";
+	}
+	
+	$dbh = mysql_connect($db_host, $db_user, $db_password) or die("Cannot connect to database: " . mysql_error());
+	mysql_select_db($db_name) or die("Cannot select database");
+	
+	$result = mysql_query($query.$restriction) or die("SQL error: ". mysql_errno());
+	$values = mysql_fetch_object($result);
+	mysql_close($dbh);
+	
+	return $values;
 }
 
-$dbh = mysql_connect($db_host, $db_user, $db_password) or die("Cannot connect to database: " . mysql_error());
-mysql_select_db($db_name) or die("Cannot select database");
+function formatValues($values) {
+	global $config;
+	
+	foreach ($values as $key => $val) {
+		if(array_key_exists($key, $config) && array_key_exists("format", $config[$key]) && $config[$key]["format"]) {
+			$values->$key= sprintf($config[$key]["format"], $val);
+			$values->$key= sprintf("%s%s", $values->$key, $config[$key]["unit"]);
+		}
+	}
 
-$result = mysql_query($query.$restriction) or die("SQL error: ". mysql_errno());
-$values = mysql_fetch_object($result);
-
-$values->OutdoorTemp = round($values->OutdoorTemp, 2)."°C";
-$values->OutdoorTemp1h = round($values->OutdoorTemp1h, 2)."°C";
-$values->OutdoorTemp24h = round($values->OutdoorTemp24h, 2)."°C";
-$values->RoomTemp = round($values->RoomTemp, 2)."°C";
-$values->RoomTemp1h = round($values->RoomTemp1h, 2)."°C";
-$values->RoomTempNominal = round($values->RoomTempNominal, 2)."°C";
-$values->HeatSourceIn = round($values->HeatSourceIn, 2)."°C";
-$values->HeatSourceOut = round($values->HeatSourceOut, 2)."°C";
-$values->SunctionGasTemp = round($values->SunctionGasTemp, 2)."°C";
-$values->OHCompressor1 = round($values->OHCompressor1, 2)."h";
-$values->CondensationTemp = round($values->CondensationTemp, 2)."°C";
-$values->CondensationPress = round($values->CondensationPress, 2)."bar";
-$values->EvaporationTemp = round($values->EvaporationTemp, 2)."°C";
-$values->EvaporationPress = round($values->EvaporationPress, 2)."bar";
-$values->FlowTemp = round($values->FlowTemp, 2)."°C";
-
-$values->CoolReturnTempNominal = round($values->CoolReturnTempNominal, 2)."°C";
-$values->CoolReturnTemp = round($values->CoolReturnTemp, 2)."°C";
-$values->HeatReturnTempNominal = round($values->HeatReturnTempNominal, 2)."°C";
-$values->DomesticWaterTempNominal = round($values->DomesticWaterTempNominal, 2)."°C";
-$values->DomesticWaterTempActual = round($values->DomesticWaterTempActual, 2)."°C";
-$values->PoolTempNominal = round($values->PoolTempNominal, 2)."°C";
-$values->PoolTempActual = round($values->PoolTempActual, 2)."°C";
-$values->SolarTempNominalMin = round($values->SolarTempNominalMin, 2)."°C";
-$values->SolarTempActual = round($values->SolarTempActual, 2)."°C";
-$values->HeatingCircuitNominal = round($values->HeatingCircuitNominal, 2)."°C";
-$values->HeatingCircuitTemp = round($values->HeatingCircuitTemp, 2)."°C";
-
-$values->HeatReturnTemp = round($values->HeatReturnTemp, 2)."°C";
-$values->OHCompressor1 = round($values->OHCompressor1, 2)."h";
-$date = new DateTime($values->RTCDate);
-$values->RTCDate = $date->format('d.m.Y');
-$time = new DateTime($values->RTCTime);
-$values->RTCTime = $time->format('H:i:s');
-$firmwareDate = new DateTime($values->FirmwareDate);
-$values->FirmwareDate = $firmwareDate->format('d.m.Y');
-$values->Pelectric=$values->Pelectric."kW";
-$values->Pthermal=$values->Pthermal."kW";
-echo json_encode($values);
+	$date = new DateTime($values->RTCDate);
+	$values->RTCDate = $date->format('d.m.Y');
+	$time = new DateTime($values->RTCTime);
+	$values->RTCTime = $time->format('H:i:s');
+	$firmwareDate = new DateTime($values->FirmwareDate);
+	$values->FirmwareDate = $firmwareDate->format('d.m.Y');
+	return($values);
+}
